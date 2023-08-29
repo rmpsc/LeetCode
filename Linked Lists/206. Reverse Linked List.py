62482485bbf9f4c97979bbbf56efc4ebc40a4e35
Romel Pascua <romelpascua14@gmail.com>
2023-08-28 23:40:49 -0700

    update reverse linked list

--- Linked Lists/206. Reverse Linked List.py
+++ Linked Lists/206. Reverse Linked List.py
@@ -17,3 +17,20 @@ class Solution:
             curr = temp
         
         return prev
+    
+'''
+recursion solution
+T: O(n) M: O(n)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
+'''
